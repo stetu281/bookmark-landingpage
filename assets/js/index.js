16a9ebe3097ee6dac718be80ceb3a036b4f69868
Stefan Turner <turner.stefan281@gmail.com>
2021-05-02 20:04:55 +0200

    Finish slider mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,30 @@ document.querySelector('.header__button').addEventListener('click', (e) => {
     document.querySelector('.header__logo').classList.toggle('header__logo--white');
     e.target.classList.toggle('header__button--close');
 })
+
+
+
+const links = document.querySelectorAll('.features__list-item');
+const slide = document.querySelector('.slider__slides');
+const actives = document.querySelectorAll('.features__link');
+
+for(let link of links) {
+    link.addEventListener('click', (e) => {
+        
+        for(let active of actives) {
+            active.classList.remove('features__link--active');
+        }     
+        
+        e.target.querySelector('.features__link').classList.add('features__link--active');
+
+        if(e.target.id === "link-one") {
+            slide.style.left = '0';
+        } else if (e.target.id === "link-two") {
+            slide.style.left = '-100%';
+        } else {
+            slide.style.left = '-200%';
+        }
+
+    })
+}
+
